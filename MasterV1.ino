5be04891fd59617e054f6b57e072615f549c154c
bhewgill <bhewgill@uvm.edu>
2017-04-13 22:47:50 -0400

    Debugged. Still needs transmit and EEPROM read.

--- MasterV1.ino
+++ MasterV1.ino
@@ -7,9 +7,11 @@ boolean testMode = true;
 #include <SPI.h>
 #include <lib_aci.h>
 #include <aci_setup.h>
-#include "services.h"
+#include <RBL_nRF8001.h>
+#include <services.h>
 
-int buttonPin = 1; // pin for sync button
+
+int buttonPin = 8; // pin for sync button
 int LED1 = 2; // Status lights
 int LED2 = 3;
 int LED3 = 4;
@@ -31,50 +33,64 @@ float sessionComp;
 
 
 void setup(){
- // Initialize what we need in here 
- sessionCount = EEPROM.read(0);
- currentAddress = EEPROM.read(2);
- // UPLOAD EEPROM SHITE
- pinMode(LED1, OUTPUT);
- pinMode(LED2, OUTPUT);
- pinMode(LED3, OUTPUT);
- if (testMode){ //start serial com
-   Serial.begin(9600);
-   Serial.print("SessionCount = ");
-   Serial.println(sessionCount);
- }
+  // Initialize what we need in here 
+  sessionCount = EEPROM.read(0);
+  currentAddress = EEPROM.read(2);
+  // UPLOAD EEPROM SHITE
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  if (testMode){ //start serial com
+    Serial.begin(9600);
+    Serial.print("SessionCount = ");
+    //Serial.println(sessionCount);
+    Serial.write(sessionCount);
+    Serial.println();
+  }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop(){
-  ArrayAdd(IntensityMap(ChannelSample()), sampleNum);
-  ButtonInterrupt();
-  
-  if (testMode){
+    if (testMode){
     Serial.println("TEST MODE ACTIVE");
-    Serial.print("SessionCount DEC ");
-    Serial.print(EEPROM.read(0),HEX);
-    Serial.print(" or HEX ");
-    Serial.println(EEPROM.read(0));
-    
-    Serial.print("currentAddress = DEC ");
-    Serial.print(currentAddress);
-    Serial.print(" or HEX ");
-    Serial.println(currentAddress, HEX);
-    
-    Serial.print("SampleNumber = "); Serial.println(sampleNum);
+    }
+    if (analogRead(sensorPin1>5) || analogRead(sensorPin2)>5){
+  ArrayAdd(IntensityMap(ChannelSample()), sampleNum);
+    }
+  ButtonInterrupt();
+
+  if (testMode){
+    Serial.print("currentAddress = HEX ");
+    Serial.write(currentAddress);
+    Serial.println();
+
+    Serial.print("SampleNumber = "); 
+    Serial.println(sampleNum);
     Serial.println("------------Loop Complete-------------");
     delay(500);
   }
   delay(500);
   if (((sampleNum+1) % 10) == 0){
-  WriteStorage();
+    WriteStorage();
   }
 }
+
+//////////////////////Create a single line of rehab data from memory///////////////////////
+
 
 //////////////////////Is the button being pressed?/////////////////////////////////////////
 void ButtonInterrupt(){
   if (digitalRead(buttonPin)){
-    // BLUETOOTH CODE HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ble_begin();
+    if (ble_connected()){
+      /////// DO BLUETOOTH THIINGS HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+    ble_do_events();
+
+    if (ble_available()){
+      while (ble_available()){
+        Serial.write(ble_read());
+      }
+    }
   }
 }
 ///////////////////////Writing to EEPROM///////////////////////////////////////////////////
@@ -104,7 +120,7 @@ void WriteStorage(){
     EEPROM.write(0,sessionCount);
   }
   if (testMode){
-    Serial.print(sessionCount);
+    Serial.write(sessionCount);
     Serial.print(',');
     Serial.print(arrayAvg);
     Serial.print(',');
@@ -115,23 +131,34 @@ void WriteStorage(){
   }
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void ArrayAdd(float intensityValue, int sampleNum){
-  sampleArray[sampleNum] = intensityValue;
-  sampleArray2[sampleNum] = intensityValue;
+void ArrayAdd(float intensityValue, int sampNum){
+  if (intensityValue>0){
+  sampleArray[sampNum] = intensityValue;
+  sampleArray2[sampNum] = intensityValue;
   sampleNum++;
   arrayTot = arrayTot + intensityValue;
   arrayTot2 = arrayTot2 + intensityValue;
-  arrayAvg = arrayTot/(sampleNum+1);
-  arrayAvg2 = arrayTot2/(sampleNum+1);
-  sessionComp = sampleNum/90;
+  arrayAvg = arrayTot/(sampNum+1);
+  arrayAvg2 = arrayTot2/(sampNum+1);
+  sessionComp = float(sampNum)/90;
+  }
+  if (testMode){
+  Serial.print("ArrayAvg = ");
+  Serial.print(arrayAvg);
+  Serial.print(" and SessionComplete = ");
+  Serial.print(sessionComp);
+  Serial.println(" percent");
+  }
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 int ChannelSample(){ // Return the highest analog input value
   int maxVolt = 0;
   int maxVolt2 = 0;
-  if (testMode && analogRead(sensorPin1) < 5){
-    Serial.println("Nothing to be read");
-  }
+//  if (testMode && ((analogRead(sensorPin1) < 5) && (analogRead(sensorPin2) < 5))){
+//    Serial.println("Nothing to be read");
+//    ButtonInterrupt();
+//    
+//  }
   while (analogRead(sensorPin1)>5 || analogRead(sensorPin2)>5){
     if (analogRead(sensorPin1)>maxVolt){
       maxVolt = analogRead(sensorPin1);
@@ -140,222 +167,225 @@ int ChannelSample(){ // Return the highest analog input value
       maxVolt2 = analogRead(sensorPin2);
     }
   }
-    return maxVolt;
+  Serial.print("MaxDigiVoltage = ");
+  Serial.println(maxVolt);
+  return maxVolt;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////
 float IntensityMap(int sensorValue){
   float intensity;
   // Nested if’s/elseif reduce processor load and speed up the cycle time
   if (sensorValue < 683)
-{
-	if (sensorValue < 393) 
-	{
-		if (sensorValue < 203)
-		{
-                    if (sensorValue < 101)
-                    {
-	            if (sensorValue < 50)
-	            {
-	            intensity = .5;
-	            }
-                    else if (sensorValue >= 50)
-                    {
-                    intensity = 1.0;
-                  }
+  {
+    if (sensorValue < 393) 
+    {
+      if (sensorValue < 203)
+      {
+        if (sensorValue < 101)
+        {
+          if (sensorValue < 50)
+          {
+            intensity = .0;
           }
-else if (sensorValue >= 101)
-{
-	if (sensorValue < 151)
-	{
-	intensity = 1.5;
-	}
-	else if (sensorValue >= 151)
-	{
-	intensity = 2.0;
-	}
-}
-		}
-		else if (sensorValue >= 203)
-		{
-			if (sensorValue < 299)
-			{
-				if (sensorValue < 251)
-				{
-				intensity = 2.5;
-				}
-				else if (sensorValue >= 251)
-				{
-				intensity = 3.0;
-				}
-			}
-			else if (sensorValue >= 299)
-			{
-				if (sensorValue < 345)
-				{
-				intensity = 3.5;
-				}
-				else if (sensorValue >= 345)
-				{
-				intensity = 4.0;
-				}
-			}
-		}
-	}
-	else if (sensorValue >= 393)
-	{
-		if (sensorValue < 594)
-		{
-			if (sensorValue < 479)
-			{
-				if (sensorValue < 438)
-				{
-				intensity = 4.5;
-				}
-				else if (sensorValue >= 438)
-				{
-				intensity = 5.0;
-				}
-			}
-			else if (sensorValue >= 479)
-			{
-				if (sensorValue < 538)
-				{
-				intensity = 5.5;
-				}
-				else if (sensorValue >= 538)
-				{
-				intensity = 6.0;
-				}
-				
-			}
-		}
-		else if (sensorValue >= 594)
-		{
-			if (sensorValue < 658)
-			{
-				if (sensorValue < 627)
-				{
-				intensity = 6.5;
-				}
-				else if (sensorValue >= 627)
-				{
-				intensity = 7.0;
-				}
-			}
-			else if (sensorValue >= 658)
-			{
-			intensity = 7.5;
-			}
-		}
-	}
-}
-else if (sensorValue >= 683)
-{
-	if (sensorValue < 876)
-	{
-		if (sensorValue < 786)
-		{
-			if (sensorValue < 737)
-			{
-				if (sensorValue < 706)
-				{
-				intensity = 8.0;
-				}
-				else if (sensorValue >= 706)
-				{
-					if (sensorValue < 724)
-					{
-					intensity = 8.5;
-					}
-					else if (sensorValue >= 724)
-					{
-					intensity = 9.0;
-					}
-				}
-			}
-			else if (sensorValue >= 737)
-			{
-				if (sensorValue < 758)
-				{
-				intensity = 9.5;
-				}
-				else if (sensorValue >= 758)
-				{
-                                intensity = 10.0;
-				}
-			}
-		}
-		else if (sensorValue >= 786)
-		{
-			if (sensorValue < 816)
-			{
-			intensity = 11.0;
-			}
-			else if (sensorValue >= 816)
-			{
-				if (sensorValue < 847)
-				{
-				intensity = 12.0;
-				}
-				else if (sensorValue >= 847)
-				{
-				intensity = 13.0;
-				}
-			}
-		}
-	}
-	else if (sensorValue >= 876)
-	{
-		if (sensorValue < 961)
-		{
-			if (sensorValue < 933)
-			{
-				if (sensorValue < 905)
-				{
-				intensity = 14.0;
-				}
-				else if (sensorValue >= 905)
-				{
-				intensity = 15.0;
-				}
-			}
-			else if (sensorValue >= 933)
-			{
-			intensity = 16.0;
-			}
-		}
-		else if (sensorValue >= 961)
-		{
-			if (sensorValue < 1010)
-			{
-				if (sensorValue < 989)
-				{
-				intensity = 17.0;
-				}
-				else if (sensorValue >= 989)
-				{
-				intensity = 18.0;
-				}
-			}
-			else if (sensorValue >= 1010)
-			{
-				if (sensorValue < 1020)
-				{
-				intensity = 19.0;
-				}
-				else if (sensorValue >= 1020)
-				{
-				intensity = 20.0;
-				}
-			}
-		}		
+          else if (sensorValue >= 50)
+          {
+            intensity = 1.0;
+          }
+        }
+        else if (sensorValue >= 101)
+        {
+          if (sensorValue < 151)
+          {
+            intensity = 1.5;
+          }
+          else if (sensorValue >= 151)
+          {
+            intensity = 2.0;
+          }
+        }
+      }
+      else if (sensorValue >= 203)
+      {
+        if (sensorValue < 299)
+        {
+          if (sensorValue < 251)
+          {
+            intensity = 2.5;
+          }
+          else if (sensorValue >= 251)
+          {
+            intensity = 3.0;
+          }
+        }
+        else if (sensorValue >= 299)
+        {
+          if (sensorValue < 345)
+          {
+            intensity = 3.5;
+          }
+          else if (sensorValue >= 345)
+          {
+            intensity = 4.0;
+          }
+        }
+      }
+    }
+    else if (sensorValue >= 393)
+    {
+      if (sensorValue < 594)
+      {
+        if (sensorValue < 479)
+        {
+          if (sensorValue < 438)
+          {
+            intensity = 4.5;
+          }
+          else if (sensorValue >= 438)
+          {
+            intensity = 5.0;
+          }
+        }
+        else if (sensorValue >= 479)
+        {
+          if (sensorValue < 538)
+          {
+            intensity = 5.5;
+          }
+          else if (sensorValue >= 538)
+          {
+            intensity = 6.0;
+          }
 
-	}
+        }
+      }
+      else if (sensorValue >= 594)
+      {
+        if (sensorValue < 658)
+        {
+          if (sensorValue < 627)
+          {
+            intensity = 6.5;
+          }
+          else if (sensorValue >= 627)
+          {
+            intensity = 7.0;
+          }
+        }
+        else if (sensorValue >= 658)
+        {
+          intensity = 7.5;
+        }
+      }
+    }
+  }
+  else if (sensorValue >= 683)
+  {
+    if (sensorValue < 876)
+    {
+      if (sensorValue < 786)
+      {
+        if (sensorValue < 737)
+        {
+          if (sensorValue < 706)
+          {
+            intensity = 8.0;
+          }
+          else if (sensorValue >= 706)
+          {
+            if (sensorValue < 724)
+            {
+              intensity = 8.5;
+            }
+            else if (sensorValue >= 724)
+            {
+              intensity = 9.0;
+            }
+          }
+        }
+        else if (sensorValue >= 737)
+        {
+          if (sensorValue < 758)
+          {
+            intensity = 9.5;
+          }
+          else if (sensorValue >= 758)
+          {
+            intensity = 10.0;
+          }
+        }
+      }
+      else if (sensorValue >= 786)
+      {
+        if (sensorValue < 816)
+        {
+          intensity = 11.0;
+        }
+        else if (sensorValue >= 816)
+        {
+          if (sensorValue < 847)
+          {
+            intensity = 12.0;
+          }
+          else if (sensorValue >= 847)
+          {
+            intensity = 13.0;
+          }
+        }
+      }
+    }
+    else if (sensorValue >= 876)
+    {
+      if (sensorValue < 961)
+      {
+        if (sensorValue < 933)
+        {
+          if (sensorValue < 905)
+          {
+            intensity = 14.0;
+          }
+          else if (sensorValue >= 905)
+          {
+            intensity = 15.0;
+          }
+        }
+        else if (sensorValue >= 933)
+        {
+          intensity = 16.0;
+        }
+      }
+      else if (sensorValue >= 961)
+      {
+        if (sensorValue < 1010)
+        {
+          if (sensorValue < 989)
+          {
+            intensity = 17.0;
+          }
+          else if (sensorValue >= 989)
+          {
+            intensity = 18.0;
+          }
+        }
+        else if (sensorValue >= 1010)
+        {
+          if (sensorValue < 1020)
+          {
+            intensity = 19.0;
+          }
+          else if (sensorValue >= 1020)
+          {
+            intensity = 20.0;
+          }
+        }
+      }		
+
+    }
+  }
+  //intensity = (intensity*4);
+  if (testMode){
+    Serial.print("Intensity = ");
+    Serial.println(intensity);
+  }
+  return intensity;
 }
-//intensity = (intensity*4);
-if (testMode){
-  Serial.print("Intensity = ");
-  Serial.println(intensity);
-}
-return intensity;
-}
+
